281b4fc2bb5fba91d60ba1aae1c6336cfa2785c4
girmameb <girmamebratu@gmail.com>
2024-07-26 13:29:10 -0700

    update file

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 -- Insert a single row into the 'Customers' table in the specified database
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
